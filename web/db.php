45ba17d42d010d34dd942a936e20ce3b9c578393
Filip Kujundzic <filipkujundzic3@gmail.com>
2023-05-19 19:42:53 +0200

    New database and web files

---
+++ web/db.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "game_shop";
+
+$connection = mysqli_connect("$host", "$username", "$password","$db") or die(mysqli_error($connection));
+
+
